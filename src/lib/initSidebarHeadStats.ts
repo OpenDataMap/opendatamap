cec1b529ce9df017fe777ab7a2749386581f18dd
labcode-de <leo@labcode.de>
2017-12-30 12:32:41 +0100

    headstats working

--- src/lib/initSidebarHeadStats.ts
+++ src/lib/initSidebarHeadStats.ts
@@ -1,7 +1,27 @@
 export function initSidebarHeadStats(nodelist) {
-    let totalNodes = 0;
+    let totalNodes = 0,
+        totalClients = 0,
+        totalOnlineNodes = 0,
+        totalGateways = 0;
     for (var key in nodelist) {
-        totalNodes = totalNodes + nodelist[key].nodes.length
+        const nodes = nodelist[key].nodes;
+        totalNodes += nodes.length;
+        for (var node in nodes) {
+            const nodestats = nodes[node].statistics;
+            const nodeflags = nodes[node].flags;
+            if (nodestats.clients !== undefined) {
+                totalClients += nodestats.clients;
+            }
+            if (nodeflags.gateway == undefined && nodeflags.online) {
+                totalOnlineNodes += 1;
+            }
+            if (nodeflags.gateway) {
+                totalGateways += 1;
+            }
+        }
     }
-    $('#sidebar-head-stats-totalnodes').html(totalNodes.toString())
+    $('#sidebar-head-stats-totalnodes').html(totalNodes.toString());
+    $('#sidebar-head-stats-totalclients').html(totalClients.toString());
+    $('#sidebar-head-stats-onlinenodes').html(totalOnlineNodes.toString());
+    $('#sidebar-head-stats-totalgateways').html(totalGateways.toString());
 }
