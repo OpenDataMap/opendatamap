50a26b28d868b7b7847a2d185f229d96f62d50ab
Caspar Armster <caspar@buchlichter.de>
2018-03-12 17:48:48 +0100

    [FIX] IoT Mapper Modul: renaming of 2 icons

--- src/lib/modules/iotmapper/addGateways.ts
+++ src/lib/modules/iotmapper/addGateways.ts
@@ -1,15 +1,15 @@
 /// <reference path="addGateways.d.ts" />
 
 import * as L from 'leaflet';
-import iconIoTMarker from './images/opendatamap_marker_iot_48px.png';
-import iconIoTMarkerShadow from './images/opendatamap_marker_iot_shadow_48px.png';
+import iconIoTGatewayMarker from './images/opendatamap_marker_iot-gateway_48px.png';
+import iconIoTGatewayMarkerShadow from './images/opendatamap_marker_iot-gateway_shadow_48px.png';
 
 export function addGateways(sourceJSON, leafletMap, leafletLayerNodes) {
     let layerIoTMapperGateways = L.layerGroup();
     const gateways = sourceJSON.gateways;
     var iconIoTMapperGateway = L.icon({
-        iconUrl: iconIoTMarker, // Anpassen an file-loader
-        shadowUrl: iconIoTMarkerShadow, // Anpassen an file-loader
+        iconUrl: iconIoTGatewayMarker, // Anpassen an file-loader
+        shadowUrl: iconIoTGatewayMarkerShadow, // Anpassen an file-loader
         iconSize:     [48, 48], // size of the icon
         shadowSize:   [48, 48], // size of the shadow
         iconAnchor:   [24, 48], // point of the icon which will correspond to marker's location
