661796ef7cc6aa97936a64eff4d11e0efd794544
Caspar Armster <caspar@buchlichter.de>
2018-03-12 18:17:19 +0100

    [FIX] IoT-Mapper Modul: only show tracking/mapping devices if the data is max 15 min old - added the if statement

--- src/lib/modules/iotmapper/addMapper.ts
+++ src/lib/modules/iotmapper/addMapper.ts
@@ -17,42 +17,44 @@ export function addMapper(sourceJSON, leafletMap, leafletLayerNodes) {
         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
     mapper.forEach((currentMapper) => {
-        const mapMapperMarker = L.marker([currentMapper.latitude, currentMapper.longitude], {icon: iconIoTMapperMapper});
-        layerIoTMapperMapper.addLayer(mapMapperMarker);
+        if(currentMapper.showOnMap) {
+            const mapMapperMarker = L.marker([currentMapper.latitude, currentMapper.longitude], {icon: iconIoTMapperMapper});
+            layerIoTMapperMapper.addLayer(mapMapperMarker);
 
-        // add Tooltip to cicle
-        mapMapperMarker.bindTooltip(currentMapper.name, {
-            className: 'leaflet-tooltip-node'
-        });
+            // add Tooltip to cicle
+            mapMapperMarker.bindTooltip(currentMapper.name, {
+                className: 'leaflet-tooltip-node'
+            });
 
-        // Settings and functions that keep the size of the nodecircle on the map
-        const myZoom = {
-            start: leafletMap.getZoom(),
-            end: leafletMap.getZoom()
-        };
+            // Settings and functions that keep the size of the nodecircle on the map
+            const myZoom = {
+                start: leafletMap.getZoom(),
+                end: leafletMap.getZoom()
+            };
 
-        leafletMap.on('zoomstart', function (e) {
-            myZoom.start = leafletMap.getZoom();
-        });
+            leafletMap.on('zoomstart', function (e) {
+                myZoom.start = leafletMap.getZoom();
+            });
 
-        leafletMap.on('zoomend', function (e) {
-            myZoom.end = leafletMap.getZoom();
-            var zoomDiff = myZoom.start - myZoom.end;
-            if (zoomDiff > 0) {
-                for (var i = 0; i < zoomDiff; i++) {
-//                    mapGatewayCircle.setRadius(mapGatewayCircle.getRadius() * 2);
+            leafletMap.on('zoomend', function (e) {
+                myZoom.end = leafletMap.getZoom();
+                var zoomDiff = myZoom.start - myZoom.end;
+                if (zoomDiff > 0) {
+                    for (var i = 0; i < zoomDiff; i++) {
+//                        mapGatewayCircle.setRadius(mapGatewayCircle.getRadius() * 2);
+                    }
+                } else if (zoomDiff < 0) {
+                    for (var i = 0; i > zoomDiff; i--) {
+//                        mapGatewayCircle.setRadius(mapGatewayCircle.getRadius() / 2);
+                    }
                 }
-            } else if (zoomDiff < 0) {
-                for (var i = 0; i > zoomDiff; i--) {
-//                    mapGatewayCircle.setRadius(mapGatewayCircle.getRadius() / 2);
-                }
-            }
-        });
+            });
 
-        // Zoom to node by clicking on it
-        mapMapperMarker.on('click', function(e: any){
-            leafletMap.setView(e.latlng, 17);
-        });
+            // Zoom to node by clicking on it
+            mapMapperMarker.on('click', function(e: any){
+                leafletMap.setView(e.latlng, 17);
+            });
+        }
     });
     if(sourceJSON.config.iotMapper) {
         layerIoTMapperMapper.addTo(leafletMap);
