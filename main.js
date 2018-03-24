4da6b272751c9a43637786a1759ba17ee7adf3e2
labcode-de <leo@labcode.de>
2018-03-24 18:22:00 +0100

    Also use dataSourceDownloader for prod (#17)

--- main.js
+++ main.js
@@ -61,6 +61,9 @@ commander
     console.log('Please wait! The assets were built')
     buildAssets();
 
+    // download data source trigger
+    downloadDataSourcesTrigger(config);
+
     // start server
     startServer(port);
   })
