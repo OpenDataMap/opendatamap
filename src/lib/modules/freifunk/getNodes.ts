6c92d29d6036309f9add1ee03cb5793088a33d79
labcode-de <leo@labcode.de>
2018-04-03 12:06:54 +0200

    closed #23 (multiple trying getNodes)

--- src/lib/modules/freifunk/getNodes.ts
+++ src/lib/modules/freifunk/getNodes.ts
@@ -2,11 +2,27 @@ import 'jquery';
 import {MD5} from "crypto-js";
 
 export function getNodes (config, cb) {
+    getData(config, function (err, nodes) {
+        if(!err) {
+            cb(nodes);
+        } else {
+            setTimeout(getData, 2000, config, function (err, nodes) {
+                if (!err) {
+                    cb(nodes);
+                } else {
+                    Materialize.toast('Problem beim Laden von ' + config.layerName, 10000);
+                    console.error('Problem with loading the layer ' + config.layerName);
+                }
+            })
+        }
+    });
+}
+
+function getData(config, cb) {
     const dataURL = "/assets/data/" + MD5(config.dataURL) + config.dataURL.substring(config.dataURL.lastIndexOf('.'));
     $.get(dataURL).done((nodes) => {
-        cb(nodes);
+        cb(null, nodes);
     }).fail((error) => {
-        Materialize.toast('Problem loading ' + config.layerName, 10000);
-        console.error('Problem with loading the layer ' + config.layerName);
+        cb("Error", null)
     })
 }
--- src/lib/modules/iotmapper/getNodes.ts
+++ src/lib/modules/iotmapper/getNodes.ts
@@ -1,8 +1,28 @@
 import 'jquery';
-import { MD5 } from 'crypto-js';
+import {MD5} from "crypto-js";
+
 export function getNodes (config, cb) {
+    getData(config, function (err, nodes) {
+        if(!err) {
+            cb(nodes);
+        } else {
+            setTimeout(getData, 2000, config, function (err, nodes) {
+                if (!err) {
+                    cb(nodes);
+                } else {
+                    Materialize.toast('Problem beim Laden von ' + config.layerName, 10000);
+                    console.error('Problem with loading the layer ' + config.layerName);
+                }
+            })
+        }
+    });
+}
+
+function getData(config, cb) {
     const dataURL = "/assets/data/" + MD5(config.dataURL) + config.dataURL.substring(config.dataURL.lastIndexOf('.'));
-    $.get(dataURL).then(function (nodes) {
-        cb(nodes);
+    $.get(dataURL).done((nodes) => {
+        cb(null, nodes);
+    }).fail((error) => {
+        cb("Error", null)
     })
 }
