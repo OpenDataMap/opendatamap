c78bc8d0ec6079b8e20a1a3a8c47d75929b27981
Leo Maroni <leo@labcode.de>
2019-02-01 16:49:20 +0100

    Fixed client-circle in freifunk module

--- src/lib/modules/freifunk/tools.ts
+++ src/lib/modules/freifunk/tools.ts
@@ -2,9 +2,10 @@ export function rotate(cx, cy, x, y, angle) {
     var radians = (Math.PI / 180) * angle,
         cos = Math.cos(radians),
         sin = Math.sin(radians),
-        stretch = Math.cos(cy),
-        nx = cx + ((cos * (x - cx)) + (sin * (y - cy))) * stretch,
-        ny = cy + ((cos * (y - cy)) - (sin * (x - cx)));
+        stretchx = Math.cos(cy),
+        stretchy = Math.cos(cx),
+        nx = cx + ((cos * (x - cx)) + (sin * (y - cy))) * stretchx,
+        ny = cy + ((cos * (y - cy)) - (sin * (x - cx))) * stretchy;
     return [nx, ny];
 }
 export function formatDate(date) {
