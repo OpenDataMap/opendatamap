c325f0d6b32a8c0f7cbed28b104d26775ba11b3b
Caspar Armster <caspar@armster.de>
2022-09-08 17:12:54 +0200

    [FIX] Fixed some specification in open311 import

--- src/lib/modules/open311/INode.ts
+++ src/lib/modules/open311/INode.ts
@@ -1,13 +1,21 @@
 interface IOpen311Node {
     service_request_id: String;
     status: String;
-    service_code: Number;
+    service_code: String;
     service_name: String;
     description: String;
     requested_datetime: String;
     updated_datetime: String;
+    first_name: String;
+    last_name: String;
     address: String;
+    address_string: String;
+    address_id: Number;
     zipcode: String;
+    email: String;
+    phone: String;
+    device_id: String;
+    account_id: String;
     latitude: Number;
     longitude: Number;
     showOnMap: Boolean;
--- src/lib/modules/open311/toJSON.ts
+++ src/lib/modules/open311/toJSON.ts
@@ -13,13 +13,21 @@ export function toJSON (config, rawNodes, cb) {
         let node = <IOpen311Node> {
             service_request_id: "",
             status: "",
-            service_code: 0,
+            service_code: "",
             service_name: "",
             description: "",
             requested_datetime: "",
             updated_datetime: "",
+            first_name: "",
+            last_name: "",
             address: "",
+            address_string: "",
+            address_id: 0,
             zipcode: "",
+            email: "",
+            phone: "",
+            device_id: "",
+            account_id: "",
             latitude: 0,
             longitude: 0,
             showOnMap: false
@@ -32,7 +40,11 @@ export function toJSON (config, rawNodes, cb) {
         node.service_request_id = currentNode.service_request_id;
         node.status = currentNode.status;
         node.service_code = currentNode.service_code;
-        node.service_name = currentNode.service_name;
+        if(currentNode.service_name == "") {
+            node.service_name = currentNode.service_code;
+        } else {
+            node.service_name = currentNode.service_name;
+        }
         node.description = currentNode.description;
         node.requested_datetime = new Date(Date.parse(currentNode.requested_datetime)).toLocaleString();
         node.updated_datetime = new Date(Date.parse(currentNode.updated_datetime)).toLocaleString();
